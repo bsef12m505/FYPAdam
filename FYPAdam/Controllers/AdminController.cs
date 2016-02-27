178f0208ef0d7c9864baac598d9dccd72f8f3ef4
bsef12m505 <bsef12m505@pucit.edu.pk>
2016-02-28 02:46:30 +0500

    faro ki stalkian

---
+++ FYPAdam/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYPAdam.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult AIndex()
+        {
+            return View();
+        }
+        public ActionResult AdminDash()
+        {
+            return View();
+        }
+        public ActionResult Charts()
+        {
+            return View();
+
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+
+        }
+        public ActionResult UserInfo()
+        {
+            return View();
+
+        }
+
+    }
+}
---
+++ FYPAdam/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FYPAdam
+{
+    public class Utilities
+    {
+        public static string EngineUrl = "http://adam.apphb.com/";
+        public static string localUrl = "http://localhost/Engine/";
+    }
+}
